607ddb61ef344f991377a3c9208d6561fc71f4cd
Administrator <Administrator@WINDOWS-UNR7VLH>
2022-04-04 20:20:43 +0530

    Employee Class & Arrays Demo

---
+++ EmployeeDemo/ArrayDemo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDemo
+{
+    class ArrayDemo
+    {
+        static void Main()
+        {
+            int[] num = new int[10];
+            Console.WriteLine("ENter Numbers");
+            for (int i = 0; i < 10; i++)
+                num[i] = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Elements are");
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(num[i]);
+
+           
+            Console.WriteLine("Elemenst using forecach loop");
+            foreach (int temp in num)
+             Console.WriteLine(temp);
+
+            // Sum & Average of the elements
+            int sum = 0;
+            foreach (int temp in num)
+                sum = sum + temp;
+            float avg = (float) sum / 10;
+            Console.WriteLine($"Sum is {sum} Average is {avg}");
+
+        }
+    }
+}
---
+++ EmployeeDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace EmployeeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee employee = new Employee();
+
+
+            //employee.GetDetails();
+            //employee.CalculateNetSalary();
+
+            //Console.WriteLine("Company Name is " + Employee.companyName);
+            //Employee.DeptDetails();
+            //Employee.ManagertDetails();
+
+            //employee.DisplayDetails();
+
+            Console.WriteLine("Company Name is"+ Employee.companyName);
+            Employee.DeptDetails();
+            Employee.ManagertDetails();
+
+            Employee emp1 = new Employee();
+            emp1.GetDetails();
+            emp1.CalculateNetSalary();
+            emp1.DisplayDetails();
+
+            Employee emp2 = new Employee();
+            emp2.GetDetails();
+            emp2.CalculateNetSalary();
+            emp2.DisplayDetails();
+
+            Employee emp3= new Employee();
+            emp3.GetDetails();
+            emp3.CalculateNetSalary();
+            emp3.DisplayDetails();
+
+
+
+
+
+
+        }
+    }
+}
